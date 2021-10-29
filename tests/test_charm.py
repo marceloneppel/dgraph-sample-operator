052f94428e3c543e6f08e89ad32ad8c3688b9fe0
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2021-10-29 15:18:04 -0300

    Add tests.

--- tests/test_charm.py
+++ tests/test_charm.py
@@ -4,7 +4,8 @@
 # Learn more about testing at: https://juju.is/docs/sdk/testing
 
 import unittest
-from unittest.mock import Mock
+from unittest.mock import Mock, patch, MagicMock
+import urllib
 
 from charm import DgraphOperatorCharm
 from ops.model import ActiveStatus
@@ -12,55 +13,132 @@ from ops.testing import Harness
 
 
 class TestCharm(unittest.TestCase):
+
     def setUp(self):
         self.harness = Harness(DgraphOperatorCharm)
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
 
-    def test_config_changed(self):
-        self.assertEqual(list(self.harness.charm._stored.things), [])
-        self.harness.update_config({"thing": "foo"})
-        self.assertEqual(list(self.harness.charm._stored.things), ["foo"])
-
-    def test_action(self):
-        # the harness doesn't (yet!) help much with actions themselves
-        action_event = Mock(params={"fail": ""})
-        self.harness.charm._on_fortune_action(action_event)
-
-        self.assertTrue(action_event.set_results.called)
-
-    def test_action_fail(self):
-        action_event = Mock(params={"fail": "fail this"})
-        self.harness.charm._on_fortune_action(action_event)
-
-        self.assertEqual(action_event.fail.call_args, [("fail this",)])
-
-    def test_httpbin_pebble_ready(self):
-        # Check the initial Pebble plan is empty
-        initial_plan = self.harness.get_container_pebble_plan("httpbin")
-        self.assertEqual(initial_plan.to_yaml(), "{}\n")
-        # Expected plan after Pebble ready with default config
-        expected_plan = {
+    def test_dgraph_layer(self):
+        # Test with empty config.
+        self.assertEqual(self.harness.charm.config["whitelist"], "")
+        expected_1 = {
+            "summary": "dgraph layer",
+            "description": "pebble config layer for dgraph",
             "services": {
-                "httpbin": {
+                "zero": {
                     "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
+                    "summary": "zero",
+                    "command": """bash -c \"set -ex; cd /data;
+                               dgraph zero --my=$(hostname -f):5080\"""",
                     "startup": "enabled",
-                    "environment": {"thing": "🎁"},
+                },
+                "alpha": {
+                    "override": "replace",
+                    "summary": "alpha",
+                    "command": """bash -c \"set -ex; cd /data;
+                               dgraph alpha --my=$(hostname -f):7080
+                               --zero $(hostname -f):5080\"""",
+                    "startup": "enabled",
                 }
             },
         }
-        # Get the httpbin container from the model
-        container = self.harness.model.unit.get_container("httpbin")
-        # Emit the PebbleReadyEvent carrying the httpbin container
-        self.harness.charm.on.httpbin_pebble_ready.emit(container)
-        # Get the plan now we've run PebbleReady
-        updated_plan = self.harness.get_container_pebble_plan("httpbin").to_dict()
-        # Check we've got the plan we expected
-        self.assertEqual(expected_plan, updated_plan)
-        # Check the service was started
-        service = self.harness.model.unit.get_container("httpbin").get_service("httpbin")
-        self.assertTrue(service.is_running())
-        # Ensure we set an ActiveStatus with no message
+        self.assertEqual(self.harness.charm._dgraph_layer(), expected_1)
+        # And now test with a different value in the whitelist config option.
+        # Disable hook firing first.
+        self.harness.disable_hooks()
+        self.harness.update_config({"whitelist": "192.168.1.16"})
+        expected_2 = {
+            "summary": "dgraph layer",
+            "description": "pebble config layer for dgraph",
+            "services": {
+                "zero": {
+                    "override": "replace",
+                    "summary": "zero",
+                    "command": """bash -c \"set -ex; cd /data;
+                               dgraph zero --my=$(hostname -f):5080\"""",
+                    "startup": "enabled",
+                },
+                "alpha": {
+                    "override": "replace",
+                    "summary": "alpha",
+                    "command": """bash -c \"set -ex; cd /data;
+                               dgraph alpha --my=$(hostname -f):7080
+                               --zero $(hostname -f):5080 --security whitelist=192.168.1.16\"""",
+                    "startup": "enabled",
+                }
+            },
+        }
+        self.assertEqual(self.harness.charm._dgraph_layer(), expected_2)
+
+    @patch('urllib.request.urlopen')
+    def test_on_config_changed(self, mock_urlopen):
+        cm = MagicMock()
+        cm.getcode.return_value = 200
+        mock_urlopen.return_value = cm
+
+        plan = self.harness.get_container_pebble_plan("dgraph")
+        self.assertEqual(plan.to_dict(), {})
+        # Trigger a config-changed hook. Since there was no plan initially, the
+        # "dgraph" service in the container won't be running so we'll be
+        # testing the `is_running() == False` codepath.
+        with patch('ops.model.Container.exec'):
+            self.harness.update_config({"whitelist": "test"})
+        plan = self.harness.get_container_pebble_plan("dgraph")
+        # Get the expected layer from the dgraph_layer method (tested above)
+        expected = self.harness.charm._dgraph_layer()
+        expected.pop("summary", "")
+        expected.pop("description", "")
+        # Check the plan is as expected
+        self.assertEqual(plan.to_dict(), expected)
         self.assertEqual(self.harness.model.unit.status, ActiveStatus())
+        container = self.harness.model.unit.get_container("dgraph")
+        self.assertEqual(container.get_service("alpha").is_running(), True)
+        self.assertEqual(container.get_service("zero").is_running(), True)
+
+        # Now test again with different config, knowing that the "dgraph"
+        # service is running (because we've just tested it above), so we'll
+        # be testing the `is_running() == True` codepath.
+        with patch('ops.model.Container.exec'):
+            self.harness.update_config({"whitelist": "192.168.1.16"})
+        plan = self.harness.get_container_pebble_plan("dgraph")
+        # Adjust the expected plan
+        expected = self.harness.charm._dgraph_layer()
+        expected.pop("summary", "")
+        expected.pop("description", "")
+        self.assertEqual(plan.to_dict()["services"]["alpha"], expected["services"]["alpha"])
+        self.assertEqual(plan.to_dict()["services"]["zero"], expected["services"]["zero"])
+        self.assertEqual(container.get_service("alpha").is_running(), True)
+        self.assertEqual(container.get_service("zero").is_running(), True)
+        self.assertEqual(self.harness.model.unit.status, ActiveStatus())
+        self.assertEqual(plan.to_dict()["services"]["alpha"], expected["services"]["alpha"])
+
+        # And finally test again with the same config to ensure we exercise
+        # the case where the plan we've created matches the active one. We're
+        # going to mock the container.stop and container.start calls to confirm
+        # they were not called.
+        with patch('ops.model.Container.exec'):
+            self.harness.charm.on.config_changed.emit()
+        with patch('ops.model.Container.start') as _start:
+            _start.assert_not_called()
+        with patch('ops.model.Container.stop') as _stop:
+            _stop.assert_not_called()
+
+    @patch("charm.DgraphOperatorCharm._fetch_data")
+    def test_on_install(self, _fetch_data):
+        self.harness.charm._on_install("mock_event")
+        _fetch_data.assert_called_once
+
+    @patch('urllib.request.urlopen')
+    def test_export_action(self, mock_urlopen):
+        magic_mock = MagicMock()
+        magic_mock.getcode.return_value = 200
+        magic_mock.read.return_value = urllib.parse.urlencode({"data": {"export": {"response":
+                                                              {"message": "Export completed.",
+                                                               "code": "Success"}}}}).encode(
+                                                                   "utf-8")
+        mock_urlopen.return_value = magic_mock
+
+        mock_event = Mock()
+        self.harness.charm._export_action(mock_event)
+        mock_event.called_once_with({"result": "database exported"})
